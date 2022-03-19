3b527ce445ce31f7c31aec8284d7cc2181c009a4
Sergio Dev <sergio-dev@outlook.com>
2022-03-19 16:59:46 +0100

    Add component state

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Mensaje from './Module.js'
 
@@ -6,16 +7,43 @@ const Description = () => {
   return <h2>La página web</h2>
 }
 
+//  Del curso 'React.js ⚛️ desde cero-Crea una aplicación...'
+
+/*  const Contador = () => {
+    const [contadorValue, actualizador] = useState(50)
+
+    return (
+    <>
+      <button onClick={() => {
+        actualizador(contadorValue + 1)
+      }}>
+        Incrementar
+      </button>
+      <span> </span>
+      <span> | </span>
+      <span> </span>
+      <button onClick={() => {
+        actualizador(contadorValue - 1)
+      }}>
+        Disminuir 
+      </button>
+      <span>{contadorValue}</span>
+    </>
+    )
+  }
+*/
+
 function App() {
-  // const mensaje = 'qué tal'
+  
 
   return (
     <div className="App">
       <Mensaje color='red' message="Estamos trabajando"/>
-      <Mensaje color='green' message=' en algo chulo'/>
+      <Mensaje color='green' message=' en algo chulo...'/>
       <Description />
-      <Mensaje color='blue' message='pero no te preocupes'/>
-
+      <Mensaje color='blue' message='...pero no te preocupes'/>
+      <br />
+      {/* <Contador /> */}
     </div>
   )
 }
