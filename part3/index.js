cd4d27d96dae03f5c339d0185c5c68153c7ec5c1
Sergio Dev <sergio-dev@outlook.com>
2022-03-24 19:46:12 +0100

    Add eslint + standard config

--- part3/index.js
+++ part3/index.js
@@ -5,20 +5,20 @@ app.use(express.json())
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2022-05-30T17:30:31.098Z',
     important: true
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2022-05-30T18:39:34.091Z',
     important: false
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-05-30T19:20:14.298Z',
     important: true
   }
 ]
@@ -73,7 +73,7 @@ app.post('/api/notes', (request, response) => {
   }
 
   notes = [...notes, newNote]
-  
+
   response.status(201).json(newNote)
 })
 
